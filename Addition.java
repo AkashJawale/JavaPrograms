834fde22ef663037fac309c5c18afca11d645f2b
AkashJawale <akashjawale131@gmail.com>
2018-10-11 20:37:01 +0530

    Java Program Addition of three numbers

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Addition
+{
+public static void main(String [] args)
+{
+int a,b,c,d;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+c=Integer.parseInt(args[2]);
+d=a+b+c;
+System.out.println("addition of three no's is="+d);
+}
+}
