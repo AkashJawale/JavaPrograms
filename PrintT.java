0c86f9da784ed93d33f7354be86ccda7cb977534
AkashJawale <akashjawale131@gmail.com>
2018-10-12 20:00:28 +0530

    Print Trangular Format

---
+++ PrintT.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class PrintT
+{
+public static void main(String s[])
+{
+int n,c;
+for(n=1;n<=4;n++)
+{
+for(c=1;c<=n;c++)
+{
+System.out.print(""+n);
+}
+System.out.print("\n");
+}
+}
+}
